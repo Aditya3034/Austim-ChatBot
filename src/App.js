499d6de8f017313138410abb9da2a2bb56a09b80
Aditya3034 <aditya3034@gmail.com>
2024-09-12 03:59:28 +0530

    fix(chatbot):fixed response handling from chatbot

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         config={config}
         messageParser={MessageParser}
         actionProvider={ActionProvider}
+        headerText="Autism Detector"
       />
     </div>
   );
